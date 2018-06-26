02f169babc03d5d72520c6be54751870bfde1074
Eric Rodrich Torres <erodrich@MacBookER.local>
2018-06-25 22:25:51 -0500

    Continuacion Controllers

--- routes/api.php
+++ routes/api.php
@@ -11,12 +11,32 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 //Route::get('ads','LocationController@index');
-Route::get('getad/{id}','AdsController@getAd');
+Route::get('getad/{id}', 'AdsController@getAd');
+//Beacon API routes
+Route::apiResource('beacons', 'BeaconController');
 //Route::post('ad','LocationController@store');
+
+/*
+Beacons
+-Mostrar todos (index)
+Route::get('beacons/', 'BeaconsController@index');
+-Mostrar $id
+Route::get('beacons/{id}', 'BeaconsController@show');
+-Actualizar $beacon
+Route::put('beacons/{id}', 'BeaconsController@show');
+-Añadir $beacon
+
+Ads
+-Mostrar todos
+-Mostrar $id
+-Actualizar $ad
+-Añadir $ad
+
+ */
